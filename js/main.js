b666e6c6099e87cf0521d1bbccfa7ab0d45cb5eb
tooneaa <47030850+tooneaa@users.noreply.github.com>
2023-05-27 21:26:00 +0800

    added Nav Bar and changed to Bootstrap

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
 // Initialize the map
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([27.57, 34.27], 13);
 
 // Add the tile layer from OpenStreetMap
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
-  maxZoom: 18
+// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//   attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
+//   maxZoom: 18
+// }).addTo(map);
+
+L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 20
 }).addTo(map);
 
 // Add a marker to the map
-var marker = L.marker([51.5, -0.09]).addTo(map);
+var marker = L.marker([27.57, 34.27]).addTo(map);
 
 // Add a popup to the marker
 marker.bindPopup("<b>Hello!</b><br>This is a Leaflet.js marker.").openPopup();
+
