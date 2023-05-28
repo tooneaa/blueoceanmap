4c275e96077b55e4a771fdfb0b5006bb5b0090a7
tooneaa <47030850+tooneaa@users.noreply.github.com>
2023-05-28 13:40:33 +0800

    waypoint text to map marker
    parsed waypoint string to an object for map marker

--- js/main.js
+++ js/main.js
@@ -1,11 +1,42 @@
+function parseWayPointStringToArrayObject(string) {
+	var trimmedString = string.trim();
+	var stringWithoutCommas = trimmedString.replace(/,/g, "");
+	var numberStrings = stringWithoutCommas.split(" ");
+
+	var numbers = numberStrings.map(function(string_num){
+		return parseFloat(string_num)
+	})
+	console.log(numbers)
+	return numbers
+
+}
+
+
 // Initialize the map
 var map = L.map('map').setView([27.57, 34.27], 13);
+var form = document.getElementById("submitWayPoint");
 
-// Add the tile layer from OpenStreetMap
-// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//   attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
-//   maxZoom: 18
-// }).addTo(map);
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  var formData = new FormData(form);
+
+  // Get the form values
+  var waypoints = formData.get("submitWayPoint");
+
+  // Do something with the form data
+  console.log(waypoints.trim());
+  console.log(typeof waypoints);
+  console.log(typeof [12.43, 0.00]);
+  waypoint_obj = parseWayPointStringToArrayObject(waypoints)
+  console.log(typeof waypoint_obj);
+  
+
+  // You can perform further operations with the data here or send it to a server using AJAX, etc.
+  L.marker(waypoint_obj).addTo(map)
+});
+
+
 
 L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
