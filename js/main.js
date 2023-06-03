54996776b6db7773642525f5acb8f820398253b6
tooneaa <47030850+tooneaa@users.noreply.github.com>
2023-06-03 22:00:08 +0800

    routes waypoint  - text field
    Text field now clears upon submission of waypoint.

--- js/main.js
+++ js/main.js
@@ -14,39 +14,38 @@ function parseWayPointStringToArrayObject(string) {
 
 }
 
+
+
 function createMultipleWayPoints(textarea) {
+	var store_waypoints = []
 	var lines = textarea.split('\n');
-	console.log("Lines");
-	console.log(lines); // Output each line separately (you can modify this based on your requirement)
-    console.log(lines.length)
+	console.log(lines)
 	for (let i = 0; i < lines.length; i++) {
 		var waypoint_line = lines[i];
-		console.log("waypt_line");
-		console.log(waypoint_line); // Output each line separately (you can modify this based on your requirement)
 		var waypoint_line_to_obj = parseWayPointStringToArrayObject(waypoint_line)
-		console.log("waypoint_line_to_obj"); // Output each line separately (you can modify this based on your requirement)
-		console.log(waypoint_line_to_obj); // Output each line separately (you can modify this based on your requirement)
+		store_waypoints.push(waypoint_line_to_obj)
+		console.log(waypoint_line_to_obj)
 		L.marker(waypoint_line_to_obj).addTo(map)
 	}
+
+	return store_waypoints
 
 }
 
 // Initialize the map
-var map = L.map('map').setView([27.57, 34.27], 13);
+var map = L.map('map').setView([27.57, 34.27], 3);
 var form = document.getElementById("submitWayPoint");
 
 form.addEventListener("submit", function(event) {
   event.preventDefault();
-
   var formData = new FormData(form);
 
   // Get the form values
   var waypoints = formData.get("submitWayPoint");
+  var waypoint_route = createMultipleWayPoints(waypoints)
+  var wayPoint_textArea = document.getElementById("exampleFormControlTextarea1")
+  wayPoint_textArea.value = ""
 
-  // Do something with the form data
-  createMultipleWayPoints(waypoints)
-
-//   console.log(typeof waypoint_obj);
   
 
   // You can perform further operations with the data here or send it to a server using AJAX, etc.
@@ -58,13 +57,22 @@ form.addEventListener("submit", function(event) {
 L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
 	subdomains: 'abcd',
-	maxZoom: 5
+	maxZoom: 20
 }).addTo(map);
 
-// Add a marker to the map
-var marker = L.marker([27.57, 34.27]).addTo(map);
 
+var start = L.marker([45, -80]).addTo(map);
+var length = 5
+var angle = 0
+var end_x = start.getLatLng().lng + length * Math.cos(angle * Math.PI / 180)
+var end_y = start.getLatLng().lat + length * Math.sin(angle * Math.PI / 180)
+var end = L.marker([end_y, end_x]).addTo(map)
+var line = L.polyline([start.getLatLng(), end.getLatLng()]).addTo(map)
+
+// Add a marker to the map
+// var marker = L.marker([27.57, 34.27]).addTo(map);
+// 
 // Add a popup to the marker
 // marker.bindPopup("<b>Hello!</b><br>This is a Leaflet.js marker.").openPopup();
-marker.bindPopup("<b>Hello!</b><br>This is a Leaflet.js marker.");
+// marker.bindPopup("<b>Hello!</b><br>This is a Leaflet.js marker.");
 
