768d164acbb90400b4da562bb8b1ef2655062e51
tooneaa <47030850+tooneaa@users.noreply.github.com>
2023-06-04 13:35:33 +0800

    seperated waypoint array and drawing function, include tooltip for waypoint
    Seperated waypoint conversion from string to arrange
    Array to Marker on his own function, with waypoint marker

--- js/main.js
+++ js/main.js
@@ -1,52 +1,64 @@
 
 
 function parseWayPointStringToArrayObject(string) {
-	var trimmedString = string.trim();
-    var trimmedString = trimmedString.replace(/ /g, "")
-
+	// in a format of "xx.xxx, xx.xxx"
+	// var trimmedString = string.trim();
+    var trimmedString = string.replace(/ /g, "")
 	var stringWithoutCommas = trimmedString.replace(/,/g, " ");
 
-	var numberStrings = stringWithoutCommas.split(" ");
-	var numbers = numberStrings.map(function(string_num){
-		return parseFloat(string_num)
-	}) 
+	var numberStringsToArray = stringWithoutCommas.split(" ");
+
+	var numbers = numberStringsToArray.map(function(string_num){
+		return parseFloat(string_num)}
+		) 
 	return numbers
-
-}
-
+} 
 
 
-function createMultipleWayPoints(textarea) {
+function convertMultipleWayPointStringsToArray(textarea) {
 	var store_waypoints = []
 	var lines = textarea.split('\n');
-	console.log(lines)
 	for (let i = 0; i < lines.length; i++) {
 		var waypoint_line = lines[i];
 		var waypoint_line_to_obj = parseWayPointStringToArrayObject(waypoint_line)
 		store_waypoints.push(waypoint_line_to_obj)
-		console.log(waypoint_line_to_obj)
-		L.marker(waypoint_line_to_obj).addTo(map)
+		// console.log(waypoint_line_to_obj)
+		// L.marker(waypoint_line_to_obj).addTo(map)
 	}
-
 	return store_waypoints
-
 }
 
-// Initialize the map
+
+function plotMultipleWayPointsFromArray(wayPointsArray) {
+	for (let i = 0; i < wayPointsArray.length; i++) {
+		var single_waypoint = wayPointsArray[i];
+		var way_point_marker = L.marker(single_waypoint).addTo(map);
+        var roundedWaypoints = single_waypoint.map(function(array_num){
+			return array_num.toFixed(3)
+		})
+		way_point_marker.bindPopup(`<b>WP ${i+1}</b></br>Lat/Long</br>${roundedWaypoints}`);
+	}
+}
+
+	// Initialize the map
 var map = L.map('map').setView([27.57, 34.27], 3);
 var form = document.getElementById("submitWayPoint");
 
+
 form.addEventListener("submit", function(event) {
-  event.preventDefault();
-  var formData = new FormData(form);
+	event.preventDefault();
+	var formData = new FormData(form);
 
-  // Get the form values
-  var waypoints = formData.get("submitWayPoint");
-  var waypoint_route = createMultipleWayPoints(waypoints)
-  var wayPoint_textArea = document.getElementById("exampleFormControlTextarea1")
-  wayPoint_textArea.value = ""
+	// Get the form values into Array
+	var waypoints = formData.get("submitWayPoint");
+	var waypointsArray = convertMultipleWayPointStringsToArray(waypoints)
 
-  
+	// Clear TextArea
+	var wayPoint_textArea = document.getElementById("exampleFormControlTextarea1")
+	wayPoint_textArea.value = ""
+
+	// plotWayPoints
+	plotMultipleWayPointsFromArray(waypointsArray)
 
   // You can perform further operations with the data here or send it to a server using AJAX, etc.
 //   L.marker(waypoint_obj).addTo(map)
