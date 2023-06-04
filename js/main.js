3e428f936185b90c607fb9a8b35f32c353f8fe84
tooneaa <47030850+tooneaa@users.noreply.github.com>
2023-06-04 16:28:40 +0800

    added lines to waypoint routes
    added polylines to waypoint routes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 
 
+var store_waypoints = Array()
+var routeMarkerList = Array()
+
+// Initialize the map
+var map = L.map('map').setView([27.57, 34.27], 3);
+var form = document.getElementById("submitWayPoint");
+
+
 function parseWayPointStringToArrayObject(string) {
 	// in a format of "xx.xxx, xx.xxx"
 	// var trimmedString = string.trim();
@@ -16,7 +24,7 @@ function parseWayPointStringToArrayObject(string) {
 
 
 function convertMultipleWayPointStringsToArray(textarea) {
-	var store_waypoints = []
+	
 	var lines = textarea.split('\n');
 	for (let i = 0; i < lines.length; i++) {
 		var waypoint_line = lines[i];
@@ -30,19 +38,44 @@ function convertMultipleWayPointStringsToArray(textarea) {
 
 
 function plotMultipleWayPointsFromArray(wayPointsArray) {
+	let routePoints = Array()
 	for (let i = 0; i < wayPointsArray.length; i++) {
 		var single_waypoint = wayPointsArray[i];
 		var way_point_marker = L.marker(single_waypoint).addTo(map);
+		
+		routePoints.push(way_point_marker)
+
         var roundedWaypoints = single_waypoint.map(function(array_num){
 			return array_num.toFixed(3)
 		})
 		way_point_marker.bindPopup(`<b>WP ${i+1}</b></br>Lat/Long</br>${roundedWaypoints}`);
 	}
+	return routeMarkerList.push(routePoints)
 }
 
-	// Initialize the map
-var map = L.map('map').setView([27.57, 34.27], 3);
-var form = document.getElementById("submitWayPoint");
+function drawPolyLineForWaypointRoute(wayPointsArray) {
+
+	for (let i=0; i < wayPointsArray.length; i++){
+		var start_point = wayPointsArray[i]
+		var end_point = wayPointsArray[i+1] 
+		var draw_line = L.polyline([start_point, end_point]).addTo(map)
+
+	}
+	
+
+}
+
+// Main Function when pressed Submit 
+function submitRouteWayPointButton(wayPointsArray) {
+    // received a array of waypoints as input
+
+	// add waypoint to map
+	plotMultipleWayPointsFromArray(wayPointsArray)
+	drawPolyLineForWaypointRoute(wayPointsArray)
+}
+
+
+
 
 
 form.addEventListener("submit", function(event) {
@@ -53,13 +86,14 @@ form.addEventListener("submit", function(event) {
 	var waypoints = formData.get("submitWayPoint");
 	var waypointsArray = convertMultipleWayPointStringsToArray(waypoints)
 
-	// Clear TextArea
-	var wayPoint_textArea = document.getElementById("exampleFormControlTextarea1")
-	wayPoint_textArea.value = ""
+	 ""
 
 	// plotWayPoints
-	plotMultipleWayPointsFromArray(waypointsArray)
+	submitRouteWayPointButton(waypointsArray)
 
+	// Clear TextArea
+	var wayPoint_textArea = document.getElementById("exampleFormControlTextarea1")
+	wayPoint_textArea.value =
   // You can perform further operations with the data here or send it to a server using AJAX, etc.
 //   L.marker(waypoint_obj).addTo(map)
 });
@@ -73,13 +107,13 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r
 }).addTo(map);
 
 
-var start = L.marker([45, -80]).addTo(map);
-var length = 5
-var angle = 0
-var end_x = start.getLatLng().lng + length * Math.cos(angle * Math.PI / 180)
-var end_y = start.getLatLng().lat + length * Math.sin(angle * Math.PI / 180)
-var end = L.marker([end_y, end_x]).addTo(map)
-var line = L.polyline([start.getLatLng(), end.getLatLng()]).addTo(map)
+// var start = L.marker([45, -80]).addTo(map);
+// var length = 5
+// var angle = 0
+// var end_x = start.getLatLng().lng + length * Math.cos(angle * Math.PI / 180)
+// var end_y = start.getLatLng().lat + length * Math.sin(angle * Math.PI / 180)
+// var end = L.marker([end_y, end_x]).addTo(map)
+// var line = L.polyline([start.getLatLng(), end.getLatLng()]).addTo(map)
 
 // Add a marker to the map
 // var marker = L.marker([27.57, 34.27]).addTo(map);
