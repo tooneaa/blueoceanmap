881e9bbb677075d682f9c5d1cc1746357b9c97e7
tooneaa <47030850+tooneaa@users.noreply.github.com>
2023-06-08 23:39:59 +0800

    submit button add a route div for storing route
    pressing submit now add new routes as a div above the create route

--- js/main.js
+++ js/main.js
@@ -5,12 +5,11 @@ var routeMarkerList = Array()
 
 // Initialize the map
 var map = L.map('map').setView([27.57, 34.27], 3);
-var form = document.getElementById("submitWayPoint");
+
 
 
 function parseWayPointStringToArrayObject(string) {
 	// in a format of "xx.xxx, xx.xxx"
-	// var trimmedString = string.trim();
     var trimmedString = string.replace(/ /g, "")
 	var stringWithoutCommas = trimmedString.replace(/,/g, " ");
 
@@ -22,9 +21,8 @@ function parseWayPointStringToArrayObject(string) {
 	return numbers
 } 
 
-
 function convertMultipleWayPointStringsToArray(textarea) {
-	
+	// loop through textarea and store it as waypoints array
 	var lines = textarea.split('\n');
 	for (let i = 0; i < lines.length; i++) {
 		var waypoint_line = lines[i];
@@ -40,6 +38,8 @@ function convertMultipleWayPointStringsToArray(textarea) {
 
 
 function plotMultipleWayPointsFromArray(wayPointsArray) {
+	// take an array of waypoints and plot it as markers.
+	// Store the routepoints in routermarkerlist
 	let routePoints = Array()
 	for (let i = 0; i < wayPointsArray.length; i++) {
 		var single_waypoint = wayPointsArray[i];
@@ -50,30 +50,89 @@ function plotMultipleWayPointsFromArray(wayPointsArray) {
         var roundedWaypoints = single_waypoint.map(function(array_num){
 			return array_num.toFixed(3)
 		})
-		way_point_marker.bindPopup(`<b>WP ${i+1}</b></br>Lat/Long</br>${roundedWaypoints}`);
+		if (i==0) {
+			way_point_marker.bindPopup(`<b>WP ${i+1}</b></br>Lat/Long</br>${roundedWaypoints}`).openPopup();
+		} else {
+			way_point_marker.bindPopup(`<b>WP ${i+1}</b></br>Lat/Long</br>${roundedWaypoints}`);
+		}
 	}
 	return routeMarkerList.push(routePoints)
 }
 
 function drawPolyLineForWaypointRoute(wayPointsArray) {
-
+	// using array of waypoints and draw lines 
 	for (let i=0; i < wayPointsArray.length -1; i++){
 		var start_point = wayPointsArray[i]
 		var end_point = wayPointsArray[i+1] 
 		var draw_line = L.polyline([start_point, end_point]).addTo(map)
-
 	}
-	
-
 }
 
+
+function addNewRouteDiv(container) {
+	var newAccordion = document.createElement('div')
+	newAccordion.classList.add("accordion", "accordion-flush")
+
+	var newAccordionItem = document.createElement('div')
+	newAccordionItem.classList.add("accordion-item")
+
+	var newButtonAccordion = document.createElement("button")
+	newButtonAccordion.classList.add("accordion-button" ,"collapsed")
+	var newButtonText = document.createTextNode("Route 1")
+	
+	newButtonAccordion.appendChild(newButtonText)
+	newAccordionItem.append(newButtonAccordion)
+	newAccordion.append(newAccordionItem)
+
+
+	var existingDiv = document.getElementById('submitroute');
+    container.insertBefore(newAccordion, existingDiv);
+
+}
+/* <div class="accordion accordion-flush" id="accordionFlushExample">
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-headingOne">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                Route - Title
+            </button>
+            </h2>
+            <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+				<div class="accordion-body">
+					<ul class="list-group">
+						<li class="list-group-item">An item</li>
+						<li class="list-group-item">A second item</li>
+						<li class="list-group-item">A third item</li>
+						<li class="list-group-item">A fourth item</li>
+						<li class="list-group-item">And a fifth one</li>
+					</ul>
+				</div>
+            </div>
+        </div>
+</div> */
+function addNewRouteElement(container) {
+	// create a new div element
+	const newDiv = document.createElement("div");
+	// and give it some content
+	const newContent = document.createTextNode("Hi there and greetings!");
+	// add the text node to the newly created div
+    
+}
+
+
 // Main Function when pressed Submit 
+var form = document.getElementById("submitWayPoint");
 function submitRouteWayPointButton(wayPointsArray) {
     // received a array of waypoints as input
 
 	// add waypoint to map
 	plotMultipleWayPointsFromArray(wayPointsArray)
+	// draw lines between waypoint
 	drawPolyLineForWaypointRoute(wayPointsArray)
+	// add routes to route list
+	// addNewRouteElement()
+	var container = document.getElementById('routes');
+	console.log(container)
+	addNewRouteDiv(container)
 }
 
 
@@ -87,8 +146,6 @@ form.addEventListener("submit", function(event) {
 	// Get the form values into Array
 	var waypoints = formData.get("submitWayPoint");
 	var waypointsArray = convertMultipleWayPointStringsToArray(waypoints)
-
-	
 
 	// plotWayPoints
 	submitRouteWayPointButton(waypointsArray)
